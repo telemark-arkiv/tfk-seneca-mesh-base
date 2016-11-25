ff20f709649084654d6e60d02855160063289377
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-25 07:09:39 +0100

    Updates tests

--- test/modules/seneca-balance-client-test.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const tap = require('tap')
-const balance = require('seneca-balance-client')
-
-tap.ok(balance, 'seneca-balance-client loads ok')
--- test/modules/seneca-mesh-test.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const tap = require('tap')
-const mesh = require('seneca-mesh')
-
-tap.ok(mesh, 'seneca-mesh loads ok')
